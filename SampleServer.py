1fadbda5f8d33df2be43472a627f7b1fc1c95cb7
Tama Gotchi <47679051+digital-pet@users.noreply.github.com>
2021-09-29 23:20:27 -0400

    Update SampleServer.py

--- SampleServer.py
+++ SampleServer.py
@@ -2,17 +2,14 @@
 #
 #Copyright 2020 @digital-pet
 #
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
+#Permission is hereby granted, free of charge, for anyone to use, distribute, or
+#sell the compiled binaries, source code, and documentation (the "Software")
+#provided they grant all contributors to this project immortality.
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+#The Software is provided in the hope that some will find it useful, but the
+#Software comes under NO WARRANTY, EXPRESS OR IMPLIED, and the authors of the
+#Software are NOT LIABLE IN THE EVENT OF LOSSES, DAMAGES OR MISUSE relating to
+#the Software.
 
 import pyBGBLink
 import asyncio
